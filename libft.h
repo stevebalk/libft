49055c8c8964b04c1414f3be0a692eacf2be55d6
stevebalk <steve.balk@web.de>
2022-12-14 16:05:03 +0100

    Added part 2 prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 18:04:27 by sbalk             #+#    #+#             */
-/*   Updated: 2022/12/14 15:05:30 by sbalk            ###   ########.fr       */
+/*   Updated: 2022/12/14 16:03:42 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,16 @@ int		ft_isprint(unsigned char ch);
 int		ft_strlen(char *str);
 int		ft_tolower(char ch);
 int		ft_toupper(char ch);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
