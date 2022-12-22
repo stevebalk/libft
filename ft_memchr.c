ceae52c7c5dda7ceae830ee1c442862e509b97f0
stevebalk <steve.balk@web.de>
2022-12-22 14:57:26 +0100

    added memchr and memcmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,43 +6,21 @@
 /*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 17:38:48 by sbalk             #+#    #+#             */
-/*   Updated: 2022/12/19 14:37:15 by sbalk            ###   ########.fr       */
+/*   Updated: 2022/12/20 23:55:50 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*src;
-	unsigned char	*ch;
-	unsigned int	i;
-	unsigned int	j;
+	const unsigned char	*src;
+	unsigned int		i;
 
-	src = (unsigned char)s;
-	ch = (unsigned char)c;
+	src = (unsigned char *)s;
 	i = 0;
-	j = 0;
-	while (n > 0)
-	{
-		if (src[i] == ch[0])
-		{
-			while(j < sizeof(int))
-			{
-				if (src[i + j] != ch[j])
-				{
-					break;
-				}
-				j++;
-			}
-			if (j == sizeof(int))
-			{
-				s = src + i;
-				return (s);
-			}
-			j = 0;
-		}
-		n--;
-	}
+	while (i++ < n)
+		if (src[i] == c)
+			return ((void *)(src + i));
 	return (NULL);
 }
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,25 @@
 /*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 16:46:27 by sbalk             #+#    #+#             */
-/*   Updated: 2022/12/19 14:39:56 by sbalk            ###   ########.fr       */
+/*   Updated: 2022/12/22 14:56:27 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char *src1;
+	unsigned char *src2;
+    size_t        i;
+
+	src1 = (unsigned char*)s1;
+	src2 = (unsigned char*)s2;
+    while (i < n)
+    {
+        if (src1[i] != src2[i])
+            return (*src1 - *src2);
+        i++;
+    }
+	return (0);
+}
