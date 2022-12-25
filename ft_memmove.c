c50bc1d79feff553f22d7fb81f70879cd01226d6
stevebalk <steve.balk@web.de>
2022-12-25 16:01:26 +0100

    created and added memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 14:43:15 by sbalk             #+#    #+#             */
-/*   Updated: 2022/12/19 14:42:00 by sbalk            ###   ########.fr       */
+/*   Updated: 2022/12/25 15:58:45 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
+	char		*d;
+	const char	*s;
+	int			i;
 
+	d = dest;
+	s = src;
+	i = 0;
+	if (d < s)
+		while (i++ < n)
+			*d++ = *s++;
+	else
+	{
+		d += (n - 1);
+		s += (n - 1);
+		while (n-- > 0)
+			*d-- = *s--;
+	}
+	return (dest);
 }
