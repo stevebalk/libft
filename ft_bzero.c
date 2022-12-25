de5b17efee8bc502e632913004fd1bb0bb835241
stevebalk <steve.balk@web.de>
2022-12-25 15:12:32 +0100

    shortened the code a bit

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:59:17 by sbalk             #+#    #+#             */
-/*   Updated: 2022/12/19 14:39:06 by sbalk            ###   ########.fr       */
+/*   Updated: 2022/12/25 15:12:07 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ void	ft_bzero(void *s, size_t size)
 	unsigned char	*temp;
 
 	temp = (unsigned char *)s;
-	while (size > 0)
+	while (size-- > 0)
 	{
 		*temp = 0;
 		temp++;
-		size--;
 	}
 	s = temp;
 }
