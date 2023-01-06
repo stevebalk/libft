ccdf32c40d1bf4820e62881660bb7472d42d2bd3
stevebalk <steve.balk@web.de>
2023-01-06 17:20:00 +0100

    tested and fixed bugs

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 13:04:02 by sbalk             #+#    #+#             */
-/*   Updated: 2023/01/05 13:35:05 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/01/06 17:16:29 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	src_size;
 	size_t	i;
 
-	src_size = ft_strlen(src);
 	i = 0;
-	while (i < size - 1 && src[i] != '\0')
+	while (i + 1 < size && src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	if (size > 0)
 		dst[i] = '\0';
-	return (src_size);
+	return (ft_strlen(src));
 }
