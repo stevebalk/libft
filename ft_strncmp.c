01affa5f3c21b595fad3218d23d193a164d6eb54
stevebalk <steve.balk@web.de>
2023-03-29 16:39:21 +0200

    Added ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,16 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
+/*   By: friendlyrobot <friendlyrobot@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 14:47:13 by sbalk             #+#    #+#             */
-/*   Updated: 2022/12/27 16:16:07 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/03/29 16:38:27 by friendlyrob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t	i;
 
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	return (0);
 }
