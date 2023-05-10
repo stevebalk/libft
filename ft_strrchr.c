b83b744f1cfce1f5d801a999ba571bff6cee1548
stevebalk <steve.balk@web.de>
2023-05-10 19:51:34 +0200

    Fixed strtrim.c and improved some others

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strrchr.c                                          :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 14:18:33 by sbalk             #+#    #+#             */
-/*   Updated: 2023/05/04 15:59:59 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/05/10 17:57:55 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Search and return a pointer to the LAST occourrence of character c
 char	*ft_strrchr(const char *s, int c)
 {
 	const char	*latest;
--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 15:55:18 by sbalk             #+#    #+#             */
-/*   Updated: 2022/12/27 16:13:04 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/05/10 17:59:48 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	new_str = malloc(sizeof(char) * (len + 1));
 	if (new_str == NULL)
 		return (NULL);
-	new_str = ft_memcpy(new_str, s + start, len);
-	new_str[len] = '\0';
+	// new_str = ft_memcpy(new_str, s + start, len);
+	new_str = ft_strlcpy(new_str, s + start, len + 1);
 	return (new_str);
 }
